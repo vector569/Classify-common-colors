0070b965c4e112d041beff6073bf7f7e9910cd01
Vector569 <aravindram.iyer6@gmail.com>
2018-07-08 20:06:04 +0530

    rename_added

--- scrape.py
+++ scrape.py
@@ -1,8 +1,10 @@
 import os
+import numpy as np
+import subprocess
 
 # Downloading individual images
 def img_download(keyword, iamge_size = "icon",N = 100):
-	command = "googleimagesdownload -k " + keyword + " -s " + image_size + " -l " + str(N)
+	command = "googleimagesdownload -k " + keyword + " -s " + image_size + " -l " + str(N) + " -f jpg"
 	os.system(command)
 
 # Download function (as an abstraction)
@@ -10,17 +12,46 @@ def download(img_list,image_size,N):
 	for img in img_list:
 		img_download(img,image_size,N)
 
+def compress(tensor,M,N):
+	(length, width,color_num) = tensor.shape
+
+def process_name(string):
+	string = str(string).strip('\'').strip('b\'')
+	ls = string.split("\\n")
+	del(ls[-1])
+	return ls
+	
+
+def names_grab(img):
+	command = "cd ~/Desktop/data/Github/Project1/downloads/" + img + " && ls"
+	proc = subprocess.Popen([command], stdout=subprocess.PIPE, shell=True)
+	(out, err) = proc.communicate()
+	list_of_names = process_name(out)
+	return list_of_names
+
+def rename(img_list):
+	for img in img_list:
+		list_of_names = names_grab(img)
+		i = 1
+		for name in list_of_names:
+			command1 = "cd ~/Desktop/data/Github/Project1/downloads/" + img + " && " 
+			command2 = "mv " + "'" + name + "'" + " " + str(i) + '.jpg'
+			command = command1 + command2
+			os.system(command)
+			i += 1
 
 if __name__ == "__main__":
 
 	img_list = ["black", "white"]  # Keywords of images to be downloaded from Google Images
-	N = 1                          # Number of images to be downloaded (N>=1)
+	N = 3                          # Number of images to be downloaded (N>=1)
 	image_size = "icon"            # Size of images to be downloaded
 	height = 4                     # Height of compressed image
 	width = 4		       # Width of compressed image
 	
 	try:
 		download(img_list,image_size,N)
+		print("\nDownload Successful\n")
+		rename(img_list)
 		#square_compression()
 
 
